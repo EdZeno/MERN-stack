3417f3d2de64db398e1215a4a91e87011aea35e0
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-05-07 12:32:46 +0300

    created components

--- game-review/src/App.js
+++ game-review/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Reviews from "./components/reviews.component.js";
+import EditReviews from "./components/editReviews.component.js";
+import CreateReviews from "./components/createReviews.component.js";
+
 
 class App extends Component {
   render() {
     return (
-
+    <Router>
         <div className="container">
           <h2>Game Review</h2>
         </div>
-
+        <Route path="/" exact component={Reviews} />
+        <Route path="/edit/:id" component={EditReviews} />
+        <Route path="/create" component={CreateReviews} />
+    </Router>
     );
   }
 }
---
+++ game-review/src/components/reviews.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Reviews extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Reviews Component!!</p>
+            </div>
+        )
+    }
+}
